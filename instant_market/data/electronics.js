db5613adce30573c424412dcf4aca9012869042e
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2024-02-04 17:28:57 +0530

    instant market

---
+++ instant_market/data/electronics.js
@@ -0,0 +1,24 @@
+const Electronics = [
+    {
+        heading: "Televisions",
+        data: ["LED - Full HD","HD Ready","LED - Ultra HD(4K)","OMLED","Curved TV"]
+    },
+    {
+        heading: "Laptops & Desktops",
+        data: ["Laptops","Desktops","2 In 1 Laptops","Foldable Laptops","Ideapads","Macbook"]
+    },
+    {
+        heading: "Cameras",
+        data: ["DSLR Cameras","SLR Cameras","Digital Cameras","Camera Lenses","Camera Accessories"]
+    },
+    {
+        heading: "Computer Accessories",
+        data: ["Office Chiar","Office Table","Conference Table","Computer Desks"]
+    },
+    {
+        heading: "Playstation Consoles",
+        data: ["Sony PS3","Sony PS4","XBox","Sony Playstation"]
+    }
+]
+
+export default Electronics
