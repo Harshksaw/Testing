1b14f9e8993eb336c75ffcf753dd4749ddb02f48
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2024-02-16 11:02:39 +0000

    logn

---
+++ login/src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+export default Navbar(){
+
+    return (
+        <nav className="fixed bottom-0 bg-black text-white w-full py-2 text-center">
+        Navbar content
+      </nav>
+    )
+}
---
+++ login/src/pages/LoginPage.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+
+function LoginPage() {
+  //   return (
+  //     <div className="h-screen  bg-gradient-to-b from-gray-900 to-indigo-700 flex flex-col w-full  overflow-hidden object-cover">
+  //       {/* Black navbar at the bottom */}
+
+  //       {/* Login box in between box */}
+  //       <div className="bg-white rounded-md shadow-md p-8 w-full max-w-md">
+  //         <h1 className="text-2xl font-bold mb-4">Login</h1>
+
+  //         {/* Form fields */}
+  //         <form action="#" method="POST">
+  //           <div className="mb-4">
+  //             <label
+  //               htmlFor="email"
+  //               className="block text-gray-700 font-bold mb-2"
+  //             >
+  //               Email
+  //             </label>
+  //             <input
+  //               type="email"
+  //               id="email"
+  //               placeholder="your@email.com"
+  //               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+  //             />
+  //           </div>
+  //           <div className="mb-6">
+  //             <label
+  //               htmlFor="password"
+  //               className="block text-gray-700 font-bold mb-2"
+  //             >
+  //               Password
+  //             </label>
+  //             <input
+  //               type="password"
+  //               id="password"
+  //               placeholder="********"
+  //               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+  //             />
+  //           </div>
+  //           <div className="flex items-center justify-between">
+  //             <button
+  //               type="submit"
+  //               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+  //             >
+  //               Login
+  //             </button>
+  //             <a
+  //               href="#"
+  //               className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-700"
+  //             >
+  //               Forgot Password?
+  //             </a>
+  //           </div>
+  //         </form>
+  //       </div>
+  //     </div>
+  //   );
+  return <div>ss</div>;
+}
+
+export default LoginPage;
