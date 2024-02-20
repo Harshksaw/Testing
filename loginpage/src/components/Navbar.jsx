8d8bfe8eaf03e405bf35eb6bb1620f0ecb538b40
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2024-02-20 13:05:54 +0530

    Add new files and update existing files

--- loginpage/src/components/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-
-
-export default function Navbar() {
-  return (
-    <nav className="fixed bottom-0 bg-black text-white w-full py-2 text-center">
-          <h1 className="text-bold text-3xl">24X7 Support</h1>
-  </nav>
-  )
-}
