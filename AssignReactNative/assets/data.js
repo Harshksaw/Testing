a2de5cdf2afa738969fe4dd7ea047b694cc1cd66
Harsh kumar <mister.harshkumar@gmail.com>
2024-04-01 22:15:12 +0530

    Added Home Carousel Filter

--- AssignReactNative/assets/data.js
+++ AssignReactNative/assets/data.js
@@ -6,97 +6,102 @@ import imageFive from "./images/five.jpeg";
 import imageSix from "./images/six.jpeg";
 import imageSeven from "./images/seven.jpeg";
 import imageEight from "./images/eight.jpeg";
-
 export const data = [
-    
     {
-        "Dish": "Paneer Tikka",
-        "RestaurantName": "Spice Garden",
-        "ImageUrl": imageOne,
-        "Type": {
-            "nonveg": false,
-            "veg": true,
-            "Indian": true,
-            "italian": false
-        }
+      "Dish": "Paneer Tikka",
+      "RestaurantName": "Spice Garden",
+      "ImageUrl": imageOne,
+      "Type": {
+        "nonveg": false,
+        "veg": true,
+        "Indian": true,
+        "italian": false,
+        "category": "restaurant" // New category property
+      }
     },
     {
-        "Dish": "Margherita Pizza",
-        "RestaurantName": "Pizza Hut",
-        "ImageUrl": imageTwo,
-        "Type": {
-            "nonveg": false,
-            "veg": true,
-            "Indian": false,
-            "italian": true
-        }
+      "Dish": "Margherita Pizza",
+      "RestaurantName": "Pizza Hut",
+      "ImageUrl": imageTwo,
+      "Type": {
+        "nonveg": false,
+        "veg": true,
+        "Indian": false,
+        "italian": true,
+        "category": "cooking" // New category property
+      }
     },
     {
-        "Dish": "Butter Chicken",
-        "RestaurantName": "Punjabi Tadka",
-        "ImageUrl": imageThree,
-        "Type": {
-            "nonveg": true,
-            "veg": false,
-            "Indian": true,
-            "italian": false
-        }
+      "Dish": "Butter Chicken",
+      "RestaurantName": "Punjabi Tadka",
+      "ImageUrl": imageThree,
+      "Type": {
+        "nonveg": true,
+        "veg": false,
+        "Indian": true,
+        "italian": false,
+        "category": "restaurant" // New category property
+      }
     },
     {
-        "Dish": "Pasta Alfredo",
-        "RestaurantName": "Café Milano",
-        "ImageUrl": imageFour,
-        "Type": {
-            "nonveg": false,
-            "veg": true,
-            "Indian": false,
-            "italian": true
-        }
+      "Dish": "Pasta Alfredo",
+      "RestaurantName": "Café Milano",
+      "ImageUrl": imageFour,
+      "Type": {
+        "nonveg": false,
+        "veg": true,
+        "Indian": false,
+        "italian": true,
+        "category": "cooking" // New category property
+      }
     },
     {
-        "Dish": "Chicken Biryani",
-        "RestaurantName": "Paradise Biryani",
-        "ImageUrl": imageFive,
-        "Type": {
-            "nonveg": true,
-            "veg": false,
-            "Indian": true,
-            "italian": false
-        }
+      "Dish": "Chicken Biryani",
+      "RestaurantName": "Paradise Biryani",
+      "ImageUrl": imageFive,
+      "Type": {
+        "nonveg": true,
+        "veg": false,
+        "Indian": true,
+        "italian": false,
+        "category": "restaurant" // New category property
+      }
     },
     {
-        "Dish": "Caprese Salad",
-        "RestaurantName": "Green Leaf",
-        "ImageUrl": imageSix,
-        "Type": {
-            "nonveg": false,
-            "veg": true,
-            "Indian": false,
-            "italian": true
-        }
+      "Dish": "Caprese Salad",
+      "RestaurantName": "Green Leaf",
+      "ImageUrl": imageSix,
+      "Type": {
+        "nonveg": false,
+        "veg": true,
+        "Indian": false,
+        "italian": true,
+        "category":  "cooking"// New category property
+      }
     },
     {
-        "Dish": "Fish Curry",
-        "RestaurantName": "Coastal Delight",
-        "ImageUrl": imageSeven,
-        "Type": {
-            "nonveg": true,
-            "veg": false,
-            "Indian": true,
-            "italian": false
-        }
+      "Dish": "Fish Curry",
+      "RestaurantName": "Coastal Delight",
+      "ImageUrl": imageSeven,
+      "Type": {
+        "nonveg": true,
+        "veg": false,
+        "Indian": true,
+        "italian": false,
+        "category": "restaurant" // New category property
+      }
     },
     {
-        "Dish": "Mushroom Risotto",
-        "RestaurantName": "Trattoria Bella",
-        "ImageUrl": imageEight,
-        "Type": {
-            "nonveg": false,
-            "veg": true,
-            "Indian": false,
-            "italian": true
-        }
+      "Dish": "Mushroom Risotto",
+      "RestaurantName": "Trattoria Bella",
+      "ImageUrl": imageEight,
+      "Type": {
+        "veg": true,
+        "nonveg": false,
+        "Indian": false,
+        "italian": true,
+        "category":  "cooking" // New category property
+      }
     }
-];
-
-
+  ];
+  
